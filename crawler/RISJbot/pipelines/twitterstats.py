28cd13d79d36a3e7d023068d47b49c959850cf5b
Ausnews Dev <dev@ausnews.org>
2021-02-16 15:16:30 +1100

    fix revoked secrets

--- crawler/RISJbot/pipelines/twitterstats.py
+++ crawler/RISJbot/pipelines/twitterstats.py
@@ -6,11 +6,11 @@ import tweepy
 logger = logging.getLogger(__name__)
 
 class TwitterRelevance:
-    api_key = "RGoakRHnlnx5Oo6Ogi0KR1Z98"
-    api_secret = "KvM3oIs3zVK9dXcKiOrRv6YCbWlRuo9YBrL73rhrFP6PRCBXu4"
-    bearer_token = "AAAAAAAAAAAAAAAAAAAAAIUyMwEAAAAAFbTFJhRL0rm6NbZt25aAPjjrLow%3DYKfU4Cziy7OMQAUihiGuBeqr37aDCoSySYYEVxriEeaJLpNe5O"
-    access_token = "1356828778257739778-o9FNZ6UrhjsDIBCOD0LFsmRw1WftPt"
-    access_token_secret = "CTLnGh5hzAg1hua7gKx9Huyl2YF2Gh4ixDV8yUoOBSghJ"
+    api_key = "TWITTER_API_KEY"
+    api_secret = "TWITTER_API_SECRET"
+    bearer_token = "TWITTER_BEARER_TOKEN"
+    access_token = "TWITTER_ACCESS_TOKEN"
+    access_token_secret = "TWITTER_ACCESS_TOKEN_SECRET"
 
     def open_spider(self, spider):
         auth = tweepy.OAuthHandler(self.api_key, self.api_secret)
