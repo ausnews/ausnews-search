a67444cf1a5e443fa3310120cab0f0e0b513616f
Ausnews Dev <dev@ausnews.org>
2021-02-17 12:07:03 +1100

    Adjust twitter weight for top news

--- site/src/app/news-search-api.service.ts
+++ site/src/app/news-search-api.service.ts
@@ -8,7 +8,7 @@ export class NewsSearchApiService {
   constructor(private http:HttpClient) { }
 
   getTopNews() {
-    const url = "https://search.ausnews.org/search?q=twitter_favourite_count%3A%3E1&h=20&ranking=twitter&twitterWeight=0.2&twitterRetweetWeight=0.2&twitterFavouriteWeight=0.1&freshnessWeight=50"
+    const url = "https://search.ausnews.org/search?q=twitter_favourite_count%3A%3E1&h=20&ranking=twitter&twitterWeight=0.5&twitterRetweetWeight=0.2&twitterFavouriteWeight=0.1&freshnessWeight=50"
     return this.http.get(url);
   }
 
