fc45e08c40330e045dcd60184bdc079b3c452e08
Ausnews Dev <dev@ausnews.org>
2021-02-19 01:25:09 +1100

    Move twitter work out of spider pipeline

---
+++ augmenter/twitter.py
@@ -0,0 +1,72 @@
+import logging
+import tweepy
+import time
+from vespa.application import Vespa
+import hashlib
+
+class TwitterInserter:
+    api_key = "TWITTER_API_KEY"
+    api_secret = "TWITTER_API_SECRET"
+    
+    def run(self):
+        self.vespa = Vespa(url = "http://vespa-search", port = 8080)
+        auth = tweepy.AppAuthHandler(self.api_key, self.api_secret)
+        self.api = tweepy.API(auth)
+        for userid in ['abcnews', 'GuardianAus', 'smh', 'iTnews_au', 'theage', 'canberratimes', 'zdnetaustralia', 'newscomauHQ', 'westaustralian']:
+            for status in self.api.user_timeline(id=userid, count=50, include_entities=True):
+                if len(status.entities['urls']) == 0:
+                    continue
+                url = status.entities['urls'][0]['expanded_url']
+                url = url.split('?')[0]
+                if (url.startswith("https://twitter.com") or url.startswith("https://zd.net") or url.startswith("https://bit.ly")):
+                    continue
+                article = self.get_article(url)
+                if article:
+                    self.update_document(article, status)
+
+    def update_document(self, article, status):
+        vespa_fields = { }
+        vespa_fields['twitter_favourite_count'] = status.favorite_count
+        vespa_fields['twitter_retweet_count'] = status.retweet_count
+        response = self.vespa.update_data(
+            schema = "newsarticle",
+            data_id = hashlib.sha256(article['fields']['url'].encode()).hexdigest(),
+            fields = vespa_fields
+        )
+        print("Updated {} with {} {}: {}".format(article['fields']['url'], status.favorite_count, status.retweet_count, response))
+
+    def get_article(self, url):
+        article_time = time.time() - 24 * 60 * 60
+        body = {
+            'yql': 'select url from sources newsarticle where userQuery();',
+            'query': "url:{}".format(url),
+            'hits': 1,
+        }
+        results = self.vespa.query(body=body)
+        if len(results.hits) > 0:
+            return results.hits[0]
+
+    def get_twitter_user(self, url):
+        if url.startswith("https://www.abc.net.au"):
+            return "abcnews"
+        if url.startswith("https://www.theguardian.com/"):
+            return "GuardianAus"
+        if url.startswith("https://www.smh.com.au"):
+            return "smh"
+        if url.startswith("https://www.itnews.com.au"):
+            return "iTnews_au"
+        if url.startswith("https://www.theage.com.au"):
+            return "theage"
+        if url.startswith("https://www.canberratimes.com.au"):
+            return "canberratimes"
+        if url.startswith("https://www.zdnet.com"):
+            return "zdnetaustralia"
+        if url.startswith("https://www.news.com.au"):
+            return "newscomauHQ"
+        if url.startswith("https://thewest.com.au"):
+            return "westaustralian"
+
+while True:
+    a = TwitterInserter()
+    a.run()
+    time.sleep(600)
