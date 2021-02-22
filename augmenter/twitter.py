dc1583a4d1a03c1d87f47730cc5a6d67f96a2aee
Ausnews Dev <dev@ausnews.org>
2021-02-23 10:28:09 +1100

    Not so noisy, augmenter

--- augmenter/twitter.py
+++ augmenter/twitter.py
@@ -55,7 +55,7 @@ class TwitterInserter:
             data_id = hashlib.sha256(article['fields']['url'].encode()).hexdigest(),
             fields = vespa_fields
         )
-        print("Updated {} with {} {}: {}".format(article['fields']['url'], status.favorite_count, status.retweet_count, response))
+        #print("Updated {} with {} {}: {}".format(article['fields']['url'], status.favorite_count, status.retweet_count, response))
 
     def get_article(self, url):
         article_time = time.time() - 24 * 60 * 60
